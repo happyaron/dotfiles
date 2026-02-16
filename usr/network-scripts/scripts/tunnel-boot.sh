9da6e4782b8afac88dbcd9f6db00d4f0968b6815
Aron Xu <happyaron.xu@gmail.com>
2026-02-17 01:04:34 +0800

    tunnel-boot: refactor

--- usr/network-scripts/scripts/tunnel-boot.sh
+++ usr/network-scripts/scripts/tunnel-boot.sh
@@ -3,21 +3,40 @@ export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
 V6ETH='eth1.100'
 V6ADDR='2001:db8::116/64'
+V6LOCAL='2001:db8::116'
+V6REMOTE='2001:db8::136'
+TUNNEL='mytunnel'
+TUNNEL_ADDR='192.0.2.65/30'
 
-detect_v6addr(){
-    if $(ip -6 addr show dev $1 | grep $2 2>&1 >/dev/null); then
-        return 0;
+apply() {
+    if ! ip -6 addr show dev "$V6ETH" | grep -q "$V6ADDR"; then
+        ip -6 addr add "$V6ADDR" dev "$V6ETH"
     fi
-    ip -6 addr add $2 dev $1
+
+    if ! ip link show "$TUNNEL" >/dev/null 2>&1; then
+        ip -6 tunnel add "$TUNNEL" mode ipip6 local "$V6LOCAL" remote "$V6REMOTE" dev "$V6ETH"
+        ip addr add dev "$TUNNEL" "$TUNNEL_ADDR"
+        ip link set "$TUNNEL" up
+    fi
 }
 
-detect_v6addr $V6ETH $V6ADDR
+teardown() {
+    if ip link show "$TUNNEL" >/dev/null 2>&1; then
+        ip link set "$TUNNEL" down
+        ip -6 tunnel del "$TUNNEL"
+    fi
+    ip -6 addr del "$V6ADDR" dev "$V6ETH" 2>/dev/null || true
+}
 
-#ipsec up vpn2-osk001
-#ip -6 tunnel add vpn2-osk001 mode ipip6 local 2001:db8::116 remote 2001:db8::704 dev eth1
-#ip addr add dev vpn2-osk001 192.0.2.7/25
-#ip link set vpn2-osk001 up
-
-ip -6 tunnel add mytunnel mode ipip6 local 2001:db8::116 remote 2001:db8::136 dev $V6ETH
-ip addr add dev mytunnel 192.0.2.65/30
-ip link set mytunnel up
+case "${1:-apply}" in
+    teardown|down|clean)
+        teardown
+        ;;
+    apply|up|"")
+        apply
+        ;;
+    *)
+        echo "Usage: $0 [apply|teardown]" >&2
+        exit 1
+        ;;
+esac
